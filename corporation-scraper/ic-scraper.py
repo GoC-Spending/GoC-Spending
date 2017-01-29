2366066f065703f858f25b3e9b66d652ad0e1f4b
Lucas Cherkewski <lucas@ecustom.ca>
2017-01-28 20:28:22 -0500

    Add an initial Industry Canada scraper for #1
    Not working properly yet, getting inconsistent results between a
    browser session and the scraper session. Also, everything's hard-
    coded for now, until we can iron out the discrepancies.

---
+++ corporation-scraper/ic-scraper.py
@@ -0,0 +1,24 @@
+import mechanicalsoup
+import urllib
+
+browser = mechanicalsoup.Browser()
+
+search_page = browser.get("https://www.ic.gc.ca/app/ccc/srch/")
+
+search_form = search_page.soup.select("#searchForm")[0]
+
+search_form.find("input", {"name": "searchCriteriaBean.textField"})["value"] = "IBM Canada Ltd."
+search_form.find("input", {"name": "searchCriteriaBean.column", "id": "company"})["checked"] = ""
+
+company_list_page = browser.submit(search_form, search_page.url)
+
+
+def is_link_to_company_details(href):
+    return (href.find('nvgt.do') is not -1) and (href.find('V_SEARCH.command=navigate') is not -1)
+
+
+company_details_link = company_list_page.soup.find_all("a", href=is_link_to_company_details)
+
+parsed_url = urllib.parse.urljoin(company_list_page.url, company_details_link[0]['href'])
+
+company_details_page = browser.get(parsed_url)
