5abf809d754f4306a9ca6fdc979ad2329b2489ba
Denis <carriere.denis@gmail.com>
2017-02-07 22:50:24 -0500

    Refactoring to ES6 A LOT better now ;) CC: @lchski

--- scrapers/scraper.js
+++ scrapers/scraper.js
@@ -1,144 +1,113 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t;
-    return { next: verb(0), "throw": verb(1), "return": verb(2) };
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = y[op[0] & 2 ? "return" : op[0] ? "throw" : "next"]) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [0, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+const fs = require('fs')
+const path = require('path')
+const request = require('request-promise')
+const entries = require('lodash').entries
+const d3 = require('d3-queue')
+const chalk = require('chalk')
+const cheerio = require('cheerio')
+const unidecode = require('unidecode')
+
+/**
+ * Login it to receive session credentials
+ *
+ * @returns {Promise<CookieJar>}
+ */
+function login () {
+  console.log('Login...')
+  const jar = request.jar()
+  return request.get('https://www.ic.gc.ca/app/ccc/srch/', {jar})
+    .then(() => jar)
+}
+
+/**
+ * HTTP Post - Get Details
+ *
+ * @param {CookieJar} jar
+ * @param {number} [offset=0] Pagination offset
+ * @returns {Promise<CookieJar, data>}
+ */
+function getDetails (jar) {
+  console.log('Get details...')
+  const offset = 5000
+  const formData = {
+    'searchCriteriaBean.textField': '*',
+    'searchCriteriaBean.column': 'nm',
+    'prtl': 1,
+    'V_SEARCH.docsStart': offset,
+    'searchCriteriaBean.hitsPerPage': 25,
+    'searchCriteriaBean.sortSpec': 'title asc',
+    'searchCriteriaBean.isSummaryOn': 'N'
+  }
+  return request.post('https://www.ic.gc.ca/app/ccc/srch/srch.do', {formData, jar})
+    .then(details => { return {details, jar} })
+}
+
+/**
+ * Get available links from details page
+ *
+ * @param {CookieJar} jar
+ * @param {HTML} details
+ * @returns {Object, jar} links {name: <href>}
+ */
+function getLinks ({jar, details}) {
+  console.log('Get links...')
+
+  const results = {}
+  const html = cheerio.load(details)
+  const links = html('ul.list-group.list-group-hover').find('a')
+  links.map((index, element) => {
+    if (element.children.length) {
+      let name = element.children[0].data.trim()
+      name = name.replace(/\//g, '-').replace('.', '')
+      name = unidecode(name)
+      name = name.toUpperCase()
+      const href = element.attribs.href
+      if (href.match(/nvgt.do/)) {
+        if (!fs.existsSync(path.join(__dirname, 'corporations', name + '.html'))) {
+          results[name] = href
+        }
+      }
     }
-};
-var fs = require("fs");
-var path = require("path");
-var request = require("request-promise");
-var cheerio = require("cheerio");
-var d3 = require("d3-queue");
-var unidecode = require('unidecode');
-// Create Folder
-if (!fs.existsSync('corporations')) {
-    fs.mkdirSync('corporations');
+  })
+  return new Promise(resolve => {
+    resolve({links: results, jar})
+  })
 }
-var thread = 0;
-function main() {
-    return __awaiter(this, void 0, void 0, function () {
-        var _this = this;
-        var jar, login, formData, search, $, links, q, start, count, errors;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    thread++;
-                    // Request Session from initial Get
-                    console.log('Starting thread...', thread);
-                    jar = request.jar();
-                    return [4 /*yield*/, request.get('https://www.ic.gc.ca/app/ccc/srch/', { jar: jar })];
-                case 1:
-                    login = _a.sent();
-                    formData = {
-                        'searchCriteriaBean.textField': '*',
-                        'searchCriteriaBean.column': 'nm',
-                        'prtl': 1,
-                        'searchCriteriaBean.hitsPerPage': 3000,
-                        'searchCriteriaBean.sortSpec': 'title asc',
-                        'searchCriteriaBean.isSummaryOn': 'N'
-                    };
-                    return [4 /*yield*/, request.post('https://www.ic.gc.ca/app/ccc/srch/srch.do', { formData: formData, jar: jar })];
-                case 2:
-                    search = _a.sent();
-                    $ = cheerio.load(search);
-                    links = $('ul.list-group.list-group-hover').find('a');
-                    q = d3.queue(5);
-                    start = new Date().getTime();
-                    count = 0;
-                    errors = 0;
-                    // Iterate over available links
-                    links.map(function (index, element) { return __awaiter(_this, void 0, void 0, function () {
-                        var _this = this;
-                        var name_1, href_1, V_TOKEN, offset_V_TOKEN_1;
-                        return __generator(this, function (_a) {
-                            if (element.children.length) {
-                                name_1 = element.children[0].data.trim();
-                                name_1 = name_1.replace(/\//g, '-').replace('.', '');
-                                name_1 = unidecode(name_1);
-                                name_1 = name_1.toUpperCase();
-                                href_1 = element.attribs.href;
-                                if (href_1.match(/nvgt.do/)) {
-                                    V_TOKEN = Number(href_1.match(/V_TOKEN=(\d*)/)[1]);
-                                    offset_V_TOKEN_1 = new Date().getTime() - V_TOKEN;
-                                    // Create new request for details
-                                    if (fs.existsSync(path.join(__dirname, 'corporations', name_1 + '.html'))) {
-                                    }
-                                    else {
-                                        q.defer(function (callback) { return __awaiter(_this, void 0, void 0, function () {
-                                            var fake_href, baseUrl, details, title;
-                                            return __generator(this, function (_a) {
-                                                switch (_a.label) {
-                                                    case 0:
-                                                        if (errors > 5) {
-                                                            return [2 /*return*/, callback(null)];
-                                                        }
-                                                        fake_href = href_1.replace(/V_TOKEN=\d*/, "V_TOKEN=" + (new Date().getTime() - offset_V_TOKEN_1));
-                                                        baseUrl = 'https://www.ic.gc.ca/app/ccc/srch/';
-                                                        return [4 /*yield*/, request.get(baseUrl + href_1, { jar: jar })];
-                                                    case 1:
-                                                        details = _a.sent();
-                                                        title = cheerio.load(details)('title').text().trim();
-                                                        if (title.match(/Error/i)) {
-                                                            errors++;
-                                                            if (errors > 5) {
-                                                                console.log('Restarting...', thread);
-                                                                main();
-                                                            }
-                                                            callback(null);
-                                                        }
-                                                        else {
-                                                            // console.log('Count:', count, 'Time:', new Date().getTime() - start)
-                                                            // count++
-                                                            console.log('Saving HTML:', "-" + name_1 + "-");
-                                                            fs.writeFileSync(path.join(__dirname, 'corporations', name_1 + '.html'), details);
-                                                            callback(null);
-                                                        }
-                                                        return [2 /*return*/];
-                                                }
-                                            });
-                                        }); });
-                                    }
-                                }
-                            }
-                            return [2 /*return*/];
-                        });
-                    }); });
-                    q.awaitAll(function (error) {
-                        console.log('done...', thread);
-                    });
-                    return [2 /*return*/];
-            }
-        });
-    });
+
+/**
+ * Get Corporation
+ *
+ * @param {Object} links {name: <href>}
+ * @param {CookieJar} jar
+ */
+function getCorporations ({links, jar}) {
+  const q = d3.queue(25)
+  for (const [name, href] of entries(links)) {
+    q.defer(callback => {
+      request.get('https://www.ic.gc.ca/app/ccc/srch/' + href, {jar}).then(details => {
+        // Parse title to check for errors
+        const title = cheerio.load(details)('title').text().trim()
+        if (title.match(/Error/i)) {
+          console.log('Error:', name)
+          callback(null)
+        } else {
+          console.log('Saving HTML:', name)
+          fs.writeFileSync(path.join(__dirname, 'corporations', name + '.html'), details)
+          callback(null)
+        }
+      })
+    })
+  }
+  q.awaitAll(() => {
+    console.log('done')
+  })
 }
-main();
+
+function main () {
+  login()
+    .then(getDetails)
+    .then(getLinks)
+    .then(getCorporations)
+}
+
+main()
