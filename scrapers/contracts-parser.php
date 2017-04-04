ef8ce694ecf0b29210bd9876bd34f37a060d0f53
Sean Boots <sean.boots@gmail.com>
2017-04-03 23:50:53 -0400

    Adding an IC parser

--- scrapers/contracts-parser.php
+++ scrapers/contracts-parser.php
@@ -17,6 +17,9 @@ if(function_exists('mb_language')) {
 	mb_language('uni'); mb_internal_encoding('UTF-8');
 }
 
+// Go crazy!
+ini_set('memory_limit', '512M');
+
 class Configuration {
 
 	public static $rawHtmlFolder = 'contracts';
@@ -27,9 +30,10 @@ class Configuration {
 		// 'agr',
 		// 'csa',
 		// 'fin',
+		// 'ic',
 	];
 
-	public static $limitDepartments = 3;
+	public static $limitDepartments = 4;
 	public static $limitFiles = 0;
 
 }
@@ -512,7 +516,7 @@ class FileParser {
 
 		preg_match_all($pattern, $html, $matches, PREG_SET_ORDER);
 
-		// var_dump($values);
+		// var_dump($matches);
 		// exit();
 
 		foreach($matches as $match) {
@@ -536,6 +540,75 @@ class FileParser {
 
 
 		}
+
+		return $values;
+
+	}
+
+	public static function ic($html) {
+
+		$html = DepartmentParser::stringBetween('<div typeof="Action">', '<p class="notPrintable">', $html);
+
+		// Remove spans that are kind of un-helpful
+		$html = str_replace([
+			'<span property="agent">',
+			'<span property="startTime">',
+			'<span property="description">',
+			'<span property="object">',
+			'</span>',
+			], '', $html);
+
+		// var_dump($html);
+		// exit();
+
+		$values = [];
+		$keyToLabel = [
+			'vendorName' => 'Vendor name:',
+			'referenceNumber' => 'Reference number:',
+			'contractDate' => 'Contract date:',
+			'description' => 'Description of work:',
+			'contractPeriodStart' => '',
+			'contractPeriodEnd' => '',
+			'contractPeriodRange' => 'Contract period / delivery date:',
+			'deliveryDate' => 'Delivery Date:',
+			'originalValue' => 'Original contract value:',
+			'contractValue' => 'Contract value:',
+			'comments' => 'Comments:',
+		];
+		$labelToKey = array_flip($keyToLabel);
+
+		$matches = [];
+		$pattern = '/<div class="ic2col1 formLeftCol">([\wÀ-ÿ\-@$#%^&+.\/,;:\s]*)<\/div>[\s]*<div class="ic2col2 formRightCol">([\wÀ-ÿ\-@$#%^&+.,;:\s]*)<\/div>/';
+
+		preg_match_all($pattern, $html, $matches, PREG_SET_ORDER);
+
+		// var_dump($matches);
+		// exit();
+
+		foreach($matches as $match) {
+
+			$label = trim(str_replace('&nbsp;', '', $match[1]));
+			$value = trim(str_replace('&nbsp;', '', $match[2]));
+
+			if(array_key_exists($label, $labelToKey)) {
+
+				$values[$labelToKey[$label]] = $value;
+
+			}
+
+		}
+
+		// Change the "to" range into start and end values:
+		if(isset($values['contractPeriodRange']) && $values['contractPeriodRange']) {
+			$split = explode(' - ', $values['contractPeriodRange']);
+			$values['contractPeriodStart'] = trim($split[0]);
+			$values['contractPeriodEnd'] = trim($split[1]);
+
+
+		}
+
+		// var_dump($values);
+		// exit();
 
 		return $values;
 
