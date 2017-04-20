5a4b4c8113a823b01267d34cf05000cf6c1b07be
Sean Boots <sean.boots@gmail.com>
2017-04-19 23:37:30 -0400

    Added parser for INFRA

--- scrapers/contracts-parser.php
+++ scrapers/contracts-parser.php
@@ -29,7 +29,7 @@ class Configuration {
 		// 'ic',
 	];
 
-	public static $limitDepartments = 4;
+	public static $limitDepartments = 0;
 	public static $limitFiles = 0;
 
 }
@@ -444,6 +444,60 @@ class FileParser {
 
 		$matches = [];
 		$pattern = '/<div class="span-2"><strong>([\wÀ-ÿ@$#%^&+\*\-.\'(),;:<\/>\s]*)<\/strong><\/div>[\s]*<div class="span-3">([\wÀ-ÿ@$#%^&+\*\-.\'(),;:<\/>\s]*)<\/div>/';
+
+		preg_match_all($pattern, $html, $matches, PREG_SET_ORDER);
+
+		// var_dump($matches);
+		// exit();
+
+		foreach($matches as $match) {
+
+			$label = trim(str_replace('&nbsp;', '', $match[1]));
+			$value = trim(str_replace('&nbsp;', '', $match[2]));
+
+			if(array_key_exists($label, $labelToKey)) {
+
+				$values[$labelToKey[$label]] = Helpers::cleanHtmlValue($value);
+
+			}
+
+		}
+
+		// Change the "to" range into start and end values:
+		if(isset($values['contractPeriodRange']) && $values['contractPeriodRange']) {
+			$split = explode(' to ', $values['contractPeriodRange']);
+			$values['contractPeriodStart'] = $split[0];
+			$values['contractPeriodEnd'] = $split[1];
+
+
+		}
+
+		return $values;
+
+	}
+
+	public static function infra($html) {
+
+		$html = Helpers::stringBetween('MainContentStart', 'MainContentEnd', $html);
+
+		$values = [];
+		$keyToLabel = [
+			'vendorName' => 'Vendor Name',
+			'referenceNumber' => 'Reference Number',
+			'contractDate' => 'Contract Date',
+			'description' => 'Description of Work',
+			'contractPeriodStart' => '',
+			'contractPeriodEnd' => '',
+			'contractPeriodRange' => 'Contract Period',
+			'deliveryDate' => 'Delivery Date',
+			'originalValue' => 'Original Contract Value',
+			'contractValue' => 'Contract Value',
+			'comments' => 'Comments',
+		];
+		$labelToKey = array_flip($keyToLabel);
+
+		$matches = [];
+		$pattern = '/<th>([\wÀ-ÿ@$#%^&+\*\-.\'(),;:\/\s]*)<\/th>[\s]*<td>([\wÀ-ÿ@$#%^&+\*\-.\'(),;:\s]*)<\/td>/';
 
 		preg_match_all($pattern, $html, $matches, PREG_SET_ORDER);
 
